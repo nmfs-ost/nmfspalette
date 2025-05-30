7451f232d837ced4bfd6efeb7e42586e6beb7814
Nicholas Tierney <njtierney@users.noreply.github.com>
2025-05-31 01:14:01 +1000

    Nick Tierney's code review (#60)
    * use_readme_rmd()
    
    * restructure README file slightly using updated `usethis` template
    * remove render-readme.yml
    * update figure locations created in README
    
    * remove namespacing (pkg::fun) from README
    
    * use knitr::convert_chunk_header
    
    * add URL and BugReports field to DESCRIPTION
    
    * ignore .octocov and .devcontainer files/folder when building the R package
    
    * use_package_doc()
    
    * ensure code does not exceed 80 character width; add two new alt regional palettes (rebasing)
    
    * splits out the ggplot2 scales into separate R file
    
    * minor tidy up of using namespacing over importFrom, and minor tidy up of documentation (adding full stops, default values, etc).
    
    * use testthat_3e
    
    * Some test improvements
    
    * use snapshot testing
    * separate out tests into a helper.R file
    * use vdiffr in tests for ggplot2
    
    * use covr and test-coverage.yaml
    
    * various fixes for R CMD Check
    
    * remove scale_name arg in discrete_scale + co
    * add globalVariables
    * use is_ggplot() over is.ggplot
    
    * address errors incurred when rebasing; move testthat and vdiffr to Suggests in DESCRIPTION
    
    * Address failing example and update documentation for scale_fill_nmfs; add global variables
    
    * Revert "use covr and test-coverage.yaml"
    
    This reverts commit 9e329af351839596747a58979ca6a8fcdb2f8467 with "git revert <hash>".
    
    This should hopefully reinstate things to the appropriate state using octocov :)
    
    ---------
    
    Co-authored-by: sbreitbart-NOAA <sophie.breitbart@noaa.gov>

---
+++ R/nmfspalette-package.R
@@ -0,0 +1,34 @@
+#' nmfspalette: A package for NOAA Fisheries color schemes
+#'
+#' The nmfspalette package provides functions to extract colors, color
+#' palettes, and use ggplot2 palettes in R.
+#'
+#' @section Details:
+#' This package contains functions to support graphing in base R and ggplot2
+#' using NOAA Fisheries colors.
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+globalVariables(c(
+  "Sepal.Length",
+  "Sepal.Width",
+  "Species",
+  "aes",
+  "cyl",
+  "cty",
+  "element_text",
+  "geom_bar",
+  "geom_point",
+  "ggplot",
+  "hp",
+  "hwy",
+  "iris",
+  "manufacturer",
+  "mpg",
+  "mtcars",
+  "theme"
+))
