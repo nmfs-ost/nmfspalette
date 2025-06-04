f93e90bb3655d35eed6f79ed58bf425390318d84
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-06-04 15:08:59 -0400

    Add option to use specific colors from nmfspalette palettes, as opposed to the default behavior (interpolation), for scale_color_nmfs; update documentation

--- R/ggplot2-scales.R
+++ R/ggplot2-scales.R
@@ -6,6 +6,9 @@
 #' Default is TRUE.
 #' @param reverse Boolean indicating whether the palette should be reversed.
 #' Default is FALSE.
+#' @param interpolate Boolean indicating whether the colors assigned to plot
+#' objects should interpolated from palettes, with the alternative that only the
+#' defined colors in the palette are used. Default is TRUE.
 #' @param ... Additional arguments passed to [ggplot2::discrete_scale()] or
 #'            [ggplot2::scale_color_gradientn()], used respectively when
 #'            `discrete` is TRUE or FALSE.
@@ -14,20 +17,34 @@
 #' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
 #'   geom_point(size = 4) +
 #'   scale_color_nmfs("coral")
+#'
+# ggplot(mtcars, aes(mpg, disp, color = as.factor(gear))) +
+#   geom_point(size = 4) +
+#   scale_color_nmfs("regional",
+#                    interpolate = FALSE,
+#                    discrete = TRUE)
 #' @export
 scale_color_nmfs <- function(
     palette = "oceans",
     discrete = TRUE,
     reverse = FALSE,
+    interpolate = TRUE,
     ...) {
   pal <- nmfs_palette(palette = palette, reverse = reverse)
 
+  pal_length <- length(nmfs_palettes[[palette]])
+
   if (discrete) {
-    ggplot2::discrete_scale(
-      aesthetics = "colour",
-      palette = pal,
-      ...
-    )
+    if (interpolate){
+      ggplot2::discrete_scale(
+        aesthetics = "colour",
+        palette = pal,
+        ...
+      )
+    } else {
+      ggplot2::scale_color_manual(
+        values = nmfs_palette(palette)(pal_length))
+    }
   } else {
     ggplot2::scale_color_gradientn(colours = pal(256), ...)
   }
