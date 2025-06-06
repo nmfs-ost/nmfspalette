98a774fac5dae913a7d6067046e9c0f381db2fb4
Sophie Breitbart <sophie.breitbart@noaa.gov>
2025-06-06 10:34:09 -0400

    Add option to use exact palette colors (#64)
    * Add option to use specific colors from nmfspalette palettes, as opposed to the default behavior (interpolation), for scale_color_nmfs; update documentation
    
    * Add interpolation option to scale_fill_nmfs; add messages if interpolate = FALSE; update documentation
    
    * Add and update tests for new interpolate argument
    
    * Make warnings displayed only once per session

--- R/ggplot2-scales.R
+++ R/ggplot2-scales.R
@@ -6,64 +6,103 @@
 #' Default is TRUE.
 #' @param reverse Boolean indicating whether the palette should be reversed.
 #' Default is FALSE.
-#' @param ... Additional arguments passed to [ggplot2::discrete_scale()] or
-#'            [ggplot2::scale_color_gradientn()], used respectively when
-#'            `discrete` is TRUE or FALSE.
+#' @param interpolate Boolean indicating whether the colors assigned to plot
+#' objects should interpolated from palettes, with the alternative that only the
+#' defined colors in the palette are used. Default is TRUE.
+#' @param ... Additional arguments passed to: [ggplot2::scale_color_gradientn()]
+#' when `discrete` is TRUE; [ggplot2::discrete_scale()] when `discrete` is FALSE
+#' and `interpolate` is TRUE; and [ggplot2::scale_color_manual()] when `discrete`
+#' is FALSE and `interpolate` is FALSE.
 #' @examples
 #' library(ggplot2)
 #' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
 #'   geom_point(size = 4) +
 #'   scale_color_nmfs("coral")
+#'
+#' ggplot(mtcars, aes(mpg, disp, color = as.factor(gear))) +
+#'   geom_point(size = 4) +
+#'   scale_color_nmfs("regional",
+#'                    interpolate = FALSE,
+#'                    discrete = TRUE)
 #' @export
 scale_color_nmfs <- function(
     palette = "oceans",
     discrete = TRUE,
     reverse = FALSE,
+    interpolate = TRUE,
     ...) {
   pal <- nmfs_palette(palette = palette, reverse = reverse)
 
+  pal_length <- length(nmfs_palettes[[palette]])
+
   if (discrete) {
-    ggplot2::discrete_scale(
-      aesthetics = "colour",
-      palette = pal,
-      ...
-    )
+    if (interpolate){
+      ggplot2::discrete_scale(
+        aesthetics = "colour",
+        palette = pal,
+        ...
+      )
+    } else {
+      cli::cli_alert_info("The {palette} palette has {pal_length} colors.")
+      rlang::warn(message = "An error will occur if there are too few palette colors for your plot.
+To avoid this error, use a larger palette or `interpolate = TRUE`.",
+                 .frequency = "once",
+                 .frequency_id = "too_few_colors_warning_color")
+      ggplot2::scale_color_manual(
+        values = nmfs_palette(palette)(pal_length),
+        ...)
+    }
   } else {
     ggplot2::scale_color_gradientn(colours = pal(256), ...)
   }
 }
 
 #' Fill scale constructor for nmfs colors
-#'
-#' @param palette Character name of palette in `nmfs_palettes`. Default value
-#' is "oceans".
-#' @param discrete Boolean indicating whether color aesthetic is discrete.
-#' Default value is TRUE.
-#' @param reverse Boolean indicating whether the palette should be reversed.
-#' Default value is FALSE.
-#' @param ... Additional arguments passed to [ggplot2::discrete_scale()] or
-#'            [ggplot2::scale_fill_gradientn()], used respectively when
-#'            `discrete` is TRUE or FALSE.
+#' @inheritParams scale_color_nmfs
+#' @param ... Additional arguments passed to: [ggplot2::scale_fill_gradientn()]
+#' when `discrete` is TRUE; [ggplot2::discrete_scale()] when `discrete` is FALSE
+#' and `interpolate` is TRUE; and [ggplot2::scale_fill_manual()] when `discrete`
+#' is FALSE and `interpolate` is FALSE.
 #' @examples
 #' library(ggplot2)
 #' ggplot(mpg, aes(x = hwy, y = cty, fill = cyl)) +
 #'   geom_point(shape = 21) +
 #'   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
 #'   scale_fill_nmfs(palette = "crustacean", discrete = FALSE)
+#'
+#' ggplot(mtcars, aes(mpg, disp, color = as.factor(gear))) +
+#'   geom_point(size = 4) +
+#'   scale_fill_nmfs("regional",
+#'                    interpolate = FALSE,
+#'                    discrete = TRUE)
 #' @export
 scale_fill_nmfs <- function(
     palette = "oceans",
     discrete = TRUE,
     reverse = FALSE,
+    interpolate = TRUE,
     ...) {
   pal <- nmfs_palette(palette = palette, reverse = reverse)
 
+  pal_length <- length(nmfs_palettes[[palette]])
+
   if (discrete) {
-    ggplot2::discrete_scale(
-      aesthetics = "fill",
-      palette = pal,
-      ...
-    )
+    if (interpolate){
+      ggplot2::discrete_scale(
+        aesthetics = "fill",
+        palette = pal,
+        ...
+      )
+    } else {
+      cli::cli_alert_info("The {palette} palette has {pal_length} colors.")
+      rlang::warn(message = "An error will occur if there are too few palette colors for your plot.
+To avoid this error, use a larger palette or `interpolate = TRUE`.",
+                  .frequency = "once",
+                  .frequency_id = "too_few_colors_warning_fill")
+      ggplot2::scale_fill_manual(
+        values = nmfs_palette(palette)(pal_length),
+        ...)
+    }
   } else {
     ggplot2::scale_fill_gradientn(colours = pal(256), ...)
   }
--- tests/testthat/test-scale-fill.R
+++ tests/testthat/test-scale-fill.R
@@ -7,16 +7,41 @@ test_that("nmfs_cols creates a valid plot", {
   )
 })
 
-p_discrete <- ggplot(mpg, aes(manufacturer, fill = manufacturer)) +
+p_discrete_interp <- ggplot(mpg, aes(manufacturer, fill = manufacturer)) +
   geom_bar() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_fill_nmfs(palette = "oceans", discrete = TRUE)
+  scale_fill_nmfs(palette = "oceans",
+                  discrete = TRUE,
+                  interpolate = TRUE)
 
 test_that("scale_fill works with discrete = TRUE", {
   skip_on_cran()
   skip_on_ci()
   vdiffr::expect_doppelganger(
     "scale_fill_nmfs with discrete = TRUE",
+    p_discrete_interp
+  )
+
+  expect_s3_class(
+    object = scale_color_nmfs(discrete = FALSE),
+    class = "ScaleContinuous"
+  )
+})
+
+p_discrete <- ggplot(mpg, aes(x = cyl,
+                              y = cty,
+                              fill = as.factor(cyl))) +
+  geom_point(shape = 22,
+             size = 3) +
+  scale_fill_nmfs(palette = "oceans",
+                  discrete = TRUE,
+                  interpolate = FALSE)
+
+test_that("scale_fill works with discrete = TRUE and interpolate = FALSE", {
+  skip_on_cran()
+  skip_on_ci()
+  vdiffr::expect_doppelganger(
+    "scale_fill_nmfs - discrete T - interp F",
     p_discrete
   )
 
